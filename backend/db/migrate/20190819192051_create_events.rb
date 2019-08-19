3bb135eea15ba56aac9c66e7f93e240687f7e627
github username <github email address>
2019-08-19 15:30:09 -0500

    working

---
+++ backend/db/migrate/20190819192051_create_events.rb
@@ -0,0 +1,21 @@
+class CreateEvents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.string :img_url
+      t.string :description
+      t.string :date
+      t.string :start_time
+      t.string :end_time
+      t.string :address
+      t.string :city
+      t.string :state
+      t.integer :zipcode
+      t.integer :price
+      t.string :lat
+      t.float :long
+      t.float :tag
+      t.integer :user_id
+    end
+  end
+end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -2,32 +2,24 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
 require 'rest-client'
-require 'pry'
 require 'json'
 
 url = 'https://www.eventbriteapi.com/v3/events/search/?q=America/New_York/'
 res = RestClient.get(url, {:Authorization => 'Bearer 64LJGGJXRM6GFCMLLQT5'})
 result = JSON.parse(res)
 
-u1 = User.create(name: "first last", email: "user1@gmail.com", password: "password")
+
+User.destroy_all
+Event.destroy_all
+
+u1 = User.create(name: "Jason Left", email: "jleft@gmail.com", password: "password")
 
 
-result['events'].each do |event|
-    Events.create(name: , img_url: , description: , )
+result["events"].each do |event|
+    name = event["name"]["text"]
+    img_url = event["logo"]["url"]
+    description = event["description"]["text"]
+
+    Event.create(name: name, img_url: img_url, description: description, date: "08-23-2019", start_time: "7pm", end_time: "11pm", address: "123 Main st", city: "Houston", state: "TX", zipcode: 77004, price: 10, tag: "party", lat: 2.8, long: -187.3, user_id: u1.id)
 end
 
-
-
-Name
-Img_url
-Description
-Date
-Start_time
-End_time
-Address
-City
-State
-Zipcode
-Price
-Likes
-Tags
