bc0a0228ebcc3b6e5f6d09ba4e612307829c6680
github username <github email address>
2019-08-20 11:42:35 -0500

    grid fix

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     var eventUrl = "http://localhost:3000/api/v1/events"
     var cont_events = document.getElementById("events-sub")
+    var tag_sort = document.getElementById("tag-display")
 
     fetch(eventUrl)
     .then(res => res.json())
@@ -27,5 +28,16 @@ document.addEventListener("DOMContentLoaded", () => {
         subDiv.append(h2, h4, h4_1)
         mainDiv.append(image, subDiv)
         cont_events.append(mainDiv)
+    }
+
+    function renderSmallCard(event) {
+        const littleDiv = document.createElement("div")
+        littleDiv.className = "littlecard shadow-lg"
+        const h4 = document.createElement("h4")
+            h4_1.innerText = event.name
+        const h6 = document.createElement("h6")
+            h6.innerText = event.address
+        littleDiv.append(h4, h6)
+        tag_sort.append(littleDiv)
     }
 })
